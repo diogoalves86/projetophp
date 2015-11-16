f781e94fd0dda91170736e1b5f857b0a790baab5
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-16 21:48:25 -0200

    Configuração inicial do banco de dados

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,11 +4,10 @@
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	
+	'connectionString' => 'mysql:host=mysql.hostinger.com.br;dbname=u993183977_adm',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'u993183977_admin',
+	'password' => 'projetofinal',
 	'charset' => 'utf8',
-	*/
 );
