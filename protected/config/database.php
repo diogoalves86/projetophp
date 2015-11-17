f078ee8f4003826aa23d4d614585f5d602195a6e
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-17 17:39:47 -0200

    Alterando string de conexão para o banco novo

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,9 +5,9 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=mysql4.000webhost.com;dbname=a3283470_projeto',
+	'connectionString' => 'pgsql:host=ec2-54-83-53-120.compute-1.amazonaws.com;port=5432;dbname=d9abmsi1cmlnj9',
 	'emulatePrepare' => true,
-	'username' => 'a3283470_admin',
-	'password' => 'projetofinal123',
+	'username' => 'dhvecwnbuqordr',
+	'password' => 'LEgJosKgbSeW6AHxGytYKRQrFm',
 	'charset' => 'utf8',
 );
