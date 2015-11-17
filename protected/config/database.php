7b5d25d174f2aed6e29ad301d5ce003dcbbbb289
Nathália <naty_souzarj@hotmail.com>
2015-11-17 20:05:17 -0200

    medidas em px

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=mysql.hostinger.com.br;dbname=u993183977_adm;dbname=u993183977_adm',
+	'connectionString' => 'mysql:host=localhost;dbname=u993183977_adm',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
