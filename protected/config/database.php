8e10773fec5eb0e2083b172c691d75335929c2d7
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-17 17:55:31 -0200

    Banco corrigido

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,9 +5,9 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'pgsql:host=ec2-54-83-53-120.compute-1.amazonaws.com;port=5432;dbname=d9abmsi1cmlnj9',
+	'connectionString' => 'mysql:host=mysql.hostinger.com.br;dbname=u993183977_adm',
 	'emulatePrepare' => true,
-	'username' => 'dhvecwnbuqordr',
-	'password' => 'LEgJosKgbSeW6AHxGytYKRQrFm',
+	'username' => 'u993183977_admin',
+	'password' => 'projetofinal',
 	'charset' => 'utf8',
 );
