fcb35fb7dddd7ebe1e230e1d65ce829111f13e4e
Nathália <naty_souzarj@hotmail.com>
2015-11-18 06:07:31 -0200

    table de boletim feita

--- protected/views/nota/boletim.php
+++ protected/views/nota/boletim.php
@@ -1,46 +1,43 @@
+<ul class="boletim-ul">
+	<li>Grau: Médio</li>
+	<li>Matrícula: @valor</li>
+	<li>Nome: @nome-usuário</li>
+	<li>Turma: @turma</li>
+	<li>Turno: </li>
+</ul>
 <table class='data-table'>
 	<thead>
 		<th>Disciplina</th>
-		<th>1ªC</th>
-			<tr>
-				<th></th>
-				<th></th>
-				<th></th>
-			</tr>
-		<th>2ªC</th>
-			<tr>
-				<th></th>
-				<th></th>
-				<th></th>
-			</tr>
-		<th>3ªC</th>
-			<tr>
-				<th></th>
-				<th></th>
-				<th></th>
-			</tr>
+		<th colspan="3">1ªC</th>
+		<th colspan="3">2ªC</th>
+		<th colspan="3">3ªC</th>
 		<th>Média Final</th>
 	</thead>
 	<tbody>
 		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
+			<td rowspan="2"></td>
+			<td colspan="1">Graus</td>
+			<td colspan="1">Rec</td>
+			<td colspan="1">Média</td>
+			<td colspan="1">Graus</td>
+			<td colspan="1">Rec</td>
+			<td colspan="1">Média</td>
+			<td colspan="1">Graus</td>
+			<td colspan="1">Rec</td>
+			<td colspan="1">Média</td>
+			<td rowspan="2"></td>
 		</tr>
 		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
+			<td rowspan="2"></td>
 		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
-		<td></td>
-		
+
 	</tbody>
 </table>
