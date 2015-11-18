48009e312ca7f6e51cdb86466c61e7eafe9e9210
Nathália <naty_souzarj@hotmail.com>
2015-11-18 13:01:48 -0200

    role index views

--- protected/views/nota/boletim.php
+++ protected/views/nota/boletim.php
@@ -1,13 +1,13 @@
 <ul class="boletim-ul">
-	<li>Grau: Médio</li>
-	<li>Matrícula: @valor</li>
-	<li>Nome: @nome-usuário</li>
-	<li>Turma: @turma</li>
+	<li>Grau: Técnico</li>
+	<li>Matrícula: 23101513</li>
+	<li>Nome: Nathália Souza Ferreira</li>
+	<li>Turma: 307</li>
 	<li>Turno: </li>
 </ul>
 <table class='data-table'>
 	<thead>
-		<th>Disciplina</th>
+		<th colspan="1">Disciplina</th>
 		<th colspan="3">1ªC</th>
 		<th colspan="3">2ªC</th>
 		<th colspan="3">3ªC</th>
@@ -15,7 +15,7 @@
 	</thead>
 	<tbody>
 		<tr>
-			<td rowspan="2"></td>
+			<td>Nomes</td>
 			<td colspan="1">Graus</td>
 			<td colspan="1">Rec</td>
 			<td colspan="1">Média</td>
@@ -30,19 +30,19 @@
 			<td colspan="1">MF</td>
 		</tr>
 		<tr>
+			<td rowspan="2">Português</td>
+			<td rowspan="2">8.5</td>
 			<td rowspan="2"></td>
+			<td rowspan="2">25.6</td>
+			<td rowspan="2">9.0</td>
 			<td rowspan="2"></td>
+			<td rowspan="2">27.0</td>
+			<td rowspan="2">7.1</td>
 			<td rowspan="2"></td>
-			<td rowspan="2"></td>
-			<td rowspan="2"></td>
-			<td rowspan="2"></td>
-			<td rowspan="2"></td>
-			<td rowspan="2"></td>
-			<td rowspan="2"></td>
+			<td rowspan="2">21.3</td>
 			<td rowspan="2"></td>
 			<td rowspan="2"></td>
 			<td rowspan="2"></td>
 		</tr>
-
 	</tbody>
 </table>
