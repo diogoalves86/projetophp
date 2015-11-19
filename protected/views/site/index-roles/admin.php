bc41305a0f5f57e24b70b1c13c7cde54ac7e3b86
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-18 22:07:32 -0200

    Removendo diretório

--- protected/views/site/index-roles/admin.php
+++
@@ -1,9 +0,0 @@
-<dl>
-	<dt>Turmas</dt>
-		<dd><a href="<?php echo $this->createUrl('turma/index')?>">Todas as turmas</a></dd>
-</dl>
-<dl>
-	<dt>Informações</dt>
-		<dd><a href="">Alunos em PAF por Turma</a></dd>
-		<dd><a href="">Checar Fale conosco</a></dd>
-</dl>
--- protected/views/site/index-roles/aluno.php
+++
@@ -1,7 +0,0 @@
-<dl>
-	<dt>Painel de Controle</dt>
-		<dd><a href="">Boletim Online</a></dd>
-		<dd><a href="">Simular Apoio</a></dd>
-	<dt>Comentários</dt>
-		<dd><a href="">Checar respostas de comentários</a></dd>
-</dl>
