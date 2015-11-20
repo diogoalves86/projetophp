fbecac08db5bce4575945e284c86a76f7a4e2042
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-20 01:01:29 -0200

    Ajuste no controlador de alunos

--- protected/controllers/AlunoController.php
+++ protected/controllers/AlunoController.php
@@ -27,9 +27,6 @@ class AlunoController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('deny',  // deny all users
-                    'users'=>array('*'),
-            ),
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'users'=>array("@"),
 			),
