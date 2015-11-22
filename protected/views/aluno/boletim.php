36b2aa4818fe634f1830e44b19613aedfb6ce2d2
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-22 14:54:48 -0200

    Ajuste na exibição do cálculo da situação do aluno

--- protected/views/aluno/boletim.php
+++ protected/views/aluno/boletim.php
@@ -31,19 +31,34 @@
 					<?php if($nota_aluno->terceira_recuperacao == null): ?>
 						<?php //Se o aluno não ficou de PFV, então sua média anual é igual a média final. ?>
 						<td></td>
-						<td><?php echo Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_aluno->terceira_certificacao) ?></td>
-						<td><?php echo Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_aluno->terceira_certificacao) ?></td>
+						<td>
+							<?php 
+								$media_anual = Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_aluno->terceira_certificacao);
+								$media_final = $media_anual;
+								echo $media_final;
+							 ?>
+						</td>
+						<td><?php echo $media_anual ?></td>
+						<td><?php echo Nota::situacaoAluno($media_final); ?></td>
 						
 					<?php else: ?>
 						<td><?php echo $nota_aluno->terceira_recuperacao ?></td>
 						<td>
 							<?php $nota_final_terceira_recuperacao = ($nota_aluno->terceira_certificacao + $nota_aluno->terceira_recuperacao) / 2 ?>
-							<?php echo Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_final_terceira_recuperacao) ?></td>
+							<?php 
+								$media_final = Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_final_terceira_recuperacao); 
+								echo $media_final;
+							?>
 						</td>
-						<td><?php echo Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_aluno->terceira_certificacao) ?></td></td>
+						<td>
+							<?php
+						 		$media_anual =  Nota::calcularMediaAnual($nota_aluno->primeira_certificacao, $nota_aluno->segunda_certificacao, $nota_aluno->terceira_certificacao);
+						 		echo $media_anual;
+						 	?>
+					 	</td>
+						<td><?php echo Nota::situacaoAluno($media_final); ?></td>
 					
 					<?php endif; ?>
-					<td><!--<?php /*echo Nota::situacaoAluno()*/ ?>--></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
