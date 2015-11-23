db395a0271693e9a2496f64ca1186f48e2e65327
marxdouglas <marx.douglas@hotmail.com>
2015-11-23 13:15:13 -0200

    Associação de professores nas turmas

--- protected/views/admin/admin.php
+++ protected/views/admin/admin.php
@@ -2,6 +2,7 @@
 	<dt>Turmas</dt>
 		<dd><a href="<?php echo $this->createUrl('turma/index')?>">Todas as turmas</a></dd>
 		<dd><a href="<?php echo $this->createUrl('turma/associarAluno')?>">Associar aluno a turma</a></dd>
+		<dd><a href="<?php echo $this->createUrl('turma/associarProfessor')?>">Associar professor a turma</a></dd>
 </dl>
 <dl>
 	<dt>Informações</dt>
